924db5fd7000a863135d226e019bd0e10524e316
gayathrigollangi <gayathrigollangii@gmail.com>
2024-11-15 21:05:25 +0530

    import and export

---
+++ JS/import export/component/navbar.js
@@ -0,0 +1,13 @@
+function navbar(){
+    return `
+        <div class="main">
+        
+            <a href="home.html">Home</a>
+            <a href="cart.html">Cart</a>
+            <a href="payment.html">Payment</a>
+        
+    </div>
+    `
+}
+//default export
+export default navbar;
