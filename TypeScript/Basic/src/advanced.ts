50403a0eb1848f69f788f260637ddbf6759119f8
gayathrigollangi <gayathrigollangii@gmail.com>
2024-12-29 22:10:52 +0530

    typescript

--- TypeScript/Basic/src/advanced.ts
+++ TypeScript/Basic/src/advanced.ts
@@ -2,3 +2,12 @@ type rgb = [number,number,number,number]
 let color : rgb =[123,256,256,102];
 
 let names : [string,string] = ["John","kevin"];
+
+
+//generic functions
+
+function getInfo<Type>(value:Type){
+    return value;
+}
+getInfo<number>(1234);
+getInfo<string>("hello");
