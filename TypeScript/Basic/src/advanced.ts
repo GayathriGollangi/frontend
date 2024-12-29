32219919c2916f68fbf5964b8dbcfc2209e24ed2
gayathrigollangi <gayathrigollangii@gmail.com>
2024-12-29 22:15:14 +0530

    typescript

--- TypeScript/Basic/src/advanced.ts
+++ TypeScript/Basic/src/advanced.ts
@@ -19,11 +19,15 @@ interface user2 {
 }
 interface Admin extends user2 {
     role: string;
+    phone1:number;
+    phone2?:number;
+    //it is not mandatory it is optional
 
 }
 let obj : Admin ={
     name: "John",
     age: 25,
-    role: "admin"
+    role: "admin",
+    phone1: 1234567890,
 
 }
