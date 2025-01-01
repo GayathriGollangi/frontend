bf039851426b4b64adeba0f1f976a610009335cd
gayathrigollangi <gayathrigollangii@gmail.com>
2025-01-01 21:33:14 +0530

    material UI

--- Material UI/1/my-app/src/App.jsx
+++ Material UI/1/my-app/src/App.jsx
@@ -4,9 +4,9 @@ import Button from '@mui/material/Button'
 const App = () => {
   return (
     <div>
-      <Button variant='contained' color='secondary'>Hello</Button>
+      <Button variant='contained' color='secondary' sx={{color:"#000", marginTop:"20px",ml:"10px"}}>Hello</Button>
       <Button variant="contained" color='error' >Warning</Button>
-
+      <p style={{color:"red", marginTop:"50px",backgroundColor:"yellow",padding:"10px",borderRadius:"10px",Typography:"h1"}}>This is error</p>
     </div>
   )
 }
