a113e0b0df72d7c12f613a51acdadb502be1a6cd
gayathrigollangi <gayathrigollangii@gmail.com>
2025-01-03 21:52:41 +0530

    material UI

--- Material UI/1/my-app/src/App.jsx
+++ Material UI/1/my-app/src/App.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import Button from '@mui/material/Button'
+import React from "react";
+import Button from "@mui/material/Button";
 
 const App = () => {
   return (
     <div>
-      <Button variant='contained' color='secondary' sx={{color:"#000", marginTop:"20px",ml:"10px"}}>Hello</Button>
-      <Button variant="contained" color='error' >Warning</Button>
-      <p style={{color:"red", marginTop:"50px",backgroundColor:"yellow",padding:"10px",borderRadius:"10px",Typography:"h1"}}>This is error</p>
+      <Button
+        variant="contained"
+        color="secondary"
+        sx={{ color: "warning.light", marginTop: "20px", ml: "10px" }}
+      >
+        Hello
+      </Button>
+      <Button variant="contained" color="error">
+        Warning
+      </Button>
+      <p
+        style={{
+          color: "red",
+          marginTop: "50px",
+          backgroundColor: "yellow",
+          padding: "10px",
+          borderRadius: "10px",
+          typography: "h1",
+          fontWeight:"h6.fontWeight"
+        }}
+      >
+        This is error
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
